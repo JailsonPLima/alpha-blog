b43f6a156f4e4e06df2fa106a7b07309f7c5c416
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2025-08-06 10:10:31 -0300

    feat: add ability to create new articles from front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,21 @@
 class ArticlesController < ApplicationController
   def show
-    binding.break # Utilizado para debug
+    # binding.break # Utilizado para debug
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    # redirect_to articles_path(@article)
+    redirect_to @article
   end
 end
