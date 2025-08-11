c55cea781a0d290d297b5c15ffe5013eea473999
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2025-08-11 08:39:38 -0300

    feat: add new shared view and config to environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove a formatação de estilo no formulário quando é gerado um erro ao criar
+# ou editar um novo artigo. Necessário reiniciar o servidor ao aplicar a mudança
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
