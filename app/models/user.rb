1fd468c22381dfda3642de78170859c2798c6178
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2025-08-12 09:43:50 -0300

    feat: convert emails to downcase before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Quando se tratar do campo de email para qualquer objeto, antes de salvá-lo
+  # no banco de dados, ele executará o método downcase abaixo.
+  before_save { self.email = email.downcase }
+
   has_many :articles
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
