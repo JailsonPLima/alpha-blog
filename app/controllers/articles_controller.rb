3d2d4468c716cfca0f7cca0fb2e0de664ecb98b7
Jailson Pinheiro Lima <jailsonp.lima03@gmail.com>
2025-08-19 13:25:47 -0300

    feat: require user and same user restrictions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,8 @@ class ArticlesController < ApplicationController
   # classe. Se pode limitar os métodos que em que será executado antes com o
   # :only
   before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_user, excpet: [ :show, :index ]
+  before_action :require_same_user, only: [ :edit, :update, :destroy ]
 
   def show
     # binding.break # Utilizado para debug
@@ -59,5 +61,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 end
